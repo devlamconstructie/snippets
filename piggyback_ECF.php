c9af534d8bf5c884550541b8bea93f78af99ae90
WdeVlam <66801041+devlamconstructie@users.noreply.github.com>
2021-05-04 11:24:06 +0200

    add attribution
    added attribution to Matthias Altman

--- piggyback_ECF.php
+++ piggyback_ECF.php
@@ -4,6 +4,7 @@
 * the point is to declare @font-face declarations *manually* in css. 
 * all this does is piggy-back on ECF plugin class being compatible with Oxygen Builder, in order to inject custom font declarations into Oxygen Builder's font selector.
 * again: you have to remember to declare the font family names, url's and weights *manually* in separate CSS files
+* this code was directly inspired by Matthias Altman's snippet found here: https://www.altmann.de/en/blog-en/code-snippet-integrate-custom-fonts-into-oxygen-en/
 */
 
 
