f7fbcb302304e20fdf0fa702b6c798f9e342dd90
Willem de Vlam <66801041+devlamconstructie@users.noreply.github.com>
2022-09-02 22:28:06 +0200

    updated for better performance and ease of use.
    now works with the PHP return value feature in oxygen builder.

--- obfuscated_email.php
+++ obfuscated_email.php
@@ -1,47 +1,48 @@
 
 <?php
 
-/**
-* encodes email adress in rot13, and prints javascript to decode again client side.
-* @param string         $email        emailadress required
-* @param boolean        $link         optional, whether to add a mailto: link. default is true/
-* @param string         $classname    optional, a classname for for the link. 
-* 
-**/
-function get_obfuscated_email($email, $link=true, $classname=null){
-	 /*obfuscated email adress */
-  $class = ($classname)? "class='$classname'" : "";
-  $str = ($link)?"<a $class href='mailto:$email' rel='nofollow' >$email</a>": $email;
- 
-  //first we encode the adress server-side using rot13. 
-	$str = str_rot13($str);
-    
-  //then we create some JS that decodes the string again.
-	ob_start();
-?>
-<script>
-  	var str = "<?= $str ?>"; 
-  	decodedstr = str.replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26)});  
-   	document.write(decodedstr);
-	</script>
-<?php
-	return ob_get_clean();
-}
-
-function obfuscated_email($email){
-	echo get_obfuscated_email($email);	
-}
-
-//example:
 /*
-print mailto email link
-obfuscated_email('a.random@emailadr.ess', true, 'my_mailto_link');
-
-//print only email adress
-obfuscated_email($email, false)
-
-//store mailto link string in variable. 
-$mailtolink = get_obfuscated_email('a.random@emailadr.ess', true, 'my_mailto_link');
-
+* encodes a mailto-link into a string and prints a span with an encoded email link in the data set, and a javascript snippet which
+* fill the container with a decoded version of the string.
+* @param string emailadress || the name of an option setting containing an adress.
+* @param string (optional) id for the span container.
+* @param string (optional) classname for the mailto link
 */
+
+function get_obfuscated_email($eml_option, $id = 'mysterious_wrapper', $class='mailto-link'){
+	
+	$eml = (filter_var($eml_option, FILTER_VALIDATE_EMAIL))? $eml_option : get_option($eml_option);
+	
+	if(!filter_var($eml, FILTER_VALIDATE_EMAIL))
+		return "no valid email found";
+	
+	$obfeml = str_rot13("<a class='$class' style='color: inherit;' href='mailto:$eml' rel='nofollow' >$eml</a>" );
+
+	$container = sprintf('<span id="%s" class="scrambled-wrapper" data-scrambled="%s"></span>',$id,$obfeml);
+
+	return $container;
+
+}
+
+
+add_action('wp_footer','print_obfuscated_email_js');
+
+function print_obfuscated_email_js(){
+	static $hasrun = false;
+
+	if ($hasrun == true)
+		return;
+
+	$hasrun = true;	
+	
+	ob_start();
+?><script>(()=>{ const wr = document.querySelectorAll('.scrambled-wrapper');for (const s of wr) {s.innerHTML = s.dataset.scrambled.replace(/[a-zA-Z]/g,c=>String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26))}})()</script><?php
+	echo ob_get_clean();
+
+}
+
+function obfuscated_email($email_option, $id=null){
+	echo get_obfuscated_email($email_option, $id);	
+}
+
 ?>
